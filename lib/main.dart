3ee9050262e5be24df83c982ad3b0668cb558e5e
kogonas <krumph3@student.gsu.edu>
2026-05-03 13:36:10 -0400

    Added full navigation routes and imports to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
+// Screens
 import 'screens/login_screen.dart';
+import 'screens/register_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/mood_screen.dart';
+import 'screens/new_entry_screen.dart';
+import 'screens/entry_list_screen.dart';
+import 'screens/entry_detail_screen.dart';
+import 'screens/insights_screen.dart';
+import 'screens/reminders_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,6 +29,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const LoginScreen(),
+      routes: {
+        "/login": (_) => const LoginScreen(),
+        "/register": (_) => const RegisterScreen(),
+        "/home": (_) => const HomeScreen(),
+        "/mood": (_) => MoodScreen(),
+        "/newEntry": (_) => const NewEntryScreen(),
+        "/entries": (_) => EntryListScreen(),
+        "/insights": (_) => InsightsScreen(),
+        "/reminders": (_) => const RemindersScreen(),
+      },
     );
   }
 }
