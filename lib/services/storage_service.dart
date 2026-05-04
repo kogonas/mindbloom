2ca2569fce16ddbb363c4c56c991a762b2fec4ee
kogonas <krumph3@student.gsu.edu>
2026-05-03 21:29:45 -0400

    Added StorageService for profile image uploads

---
+++ lib/services/storage_service.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StorageService {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  Future<String> uploadProfileImage(String uid, File file) async {
+    final ref = _storage.ref().child("profile_images/$uid.jpg");
+
+    await ref.putFile(file);
+
+    return await ref.getDownloadURL();
+  }
+}
